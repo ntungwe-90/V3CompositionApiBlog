190ca8282d1836c470e9cf47f60c00a3aee5cf4e
unknown <you@example.com>
2024-01-18 16:55:40 +0000

    vue3 composition API Blog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>homePage</p>
+    <div v-if="error">{{ error }}</div>
+    <PostList :posts="posts" />
+    <!-- <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">delete a post</button> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { PostList },
+  setup() {
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+    load()
+    return { posts }
   }
+
 }
 </script>
+
+<style scoped>
+.home {
+  background: teal;
+}
+</style>
